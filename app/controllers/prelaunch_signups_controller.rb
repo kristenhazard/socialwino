378a75a6c595f0894cecf635daacf5f2494a242f
Kristen Hazard <kristen@hazardbio.com>
2010-08-04 17:35:44 -0700

    duplicate code be gone! utilize the layout

--- app/controllers/prelaunch_signups_controller.rb
+++ app/controllers/prelaunch_signups_controller.rb
@@ -4,31 +4,12 @@ class PrelaunchSignupsController < ApplicationController
   
   layout "prelaunch_signup"
   
-  def winemaker
-    @prelaunch_signup = PrelaunchSignup.new
-  end
-
-  
   def index
     @prelaunch_signups = PrelaunchSignup.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @prelaunch_signups }
-    end
-  end
-  
-  def landing
-
   end
 
   def show
     @prelaunch_signup = PrelaunchSignup.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @prelaunch_signup }
-    end
   end
 
   def new
@@ -41,39 +22,36 @@ class PrelaunchSignupsController < ApplicationController
 
   def create
     @prelaunch_signup = PrelaunchSignup.new(params[:prelaunch_signup])
-
-    respond_to do |format|
-      if @prelaunch_signup.save
-        format.html { redirect_to(@prelaunch_signup, :notice => 'Prelaunch signup was successfully created.') }
-        format.xml  { render :xml => @prelaunch_signup, :status => :created, :location => @prelaunch_signup }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @prelaunch_signup.errors, :status => :unprocessable_entity }
-      end
+    if @prelaunch_signup.save
+      redirect_to(@prelaunch_signup, :notice => 'Prelaunch signup was successfully created.') 
+    else
+      render :action => "new" 
     end
   end
 
   def update
     @prelaunch_signup = PrelaunchSignup.find(params[:id])
-
-    respond_to do |format|
-      if @prelaunch_signup.update_attributes(params[:prelaunch_signup])
-        format.html { redirect_to(@prelaunch_signup, :notice => 'Prelaunch signup was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @prelaunch_signup.errors, :status => :unprocessable_entity }
-      end
+    if @prelaunch_signup.update_attributes(params[:prelaunch_signup])
+      redirect_to(@prelaunch_signup, :notice => 'Prelaunch signup was successfully updated.') 
+    else
+      render :action => "edit" 
     end
   end
 
   def destroy
     @prelaunch_signup = PrelaunchSignup.find(params[:id])
     @prelaunch_signup.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(prelaunch_signups_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(prelaunch_signups_url)
   end
+  
+  # landing page is static, doesn't need to be part of prelaunch_signups
+  def landing
+
+  end
+  
+  # separate new page for winemakers so I can track on google analytics
+  def winemaker
+    @prelaunch_signup = PrelaunchSignup.new
+  end
+  
 end
