3627547df935392458e959001fabc8ab8fcc8a3a
Ken Rothmuller <krothmuller@gmail.com>
2010-12-03 13:54:26 -0800

    live tweets and wine menu

--- public/javascripts/app/main.js
+++ public/javascripts/app/main.js
@@ -6,20 +6,25 @@ $(function () {
   var scrollLeft = 0;
   const TWEET_WIDTH = 200;
   var tweets ;
+  var paused = false;
   console.log ("DOM Ready");
   
   // Gather Sample Tweet Set
   getTweets ();
   scrollTweets ();
+  setupWineMenu ();
   pourTweet();
   
   function getTweets () {
     var $divs
-    tweets = new Tweets (sample);
-      $divs = tweets.oldest(10, "div");
+    // tweets = new Tweets (sample, ...)
+    tweets = new Tweets (0, function (status) {
+      console.log ("Callback Executed, build visual tweet stream - status: " + status)
+      $divs = tweets.oldestNTweets(10, "div");
       $.each ($divs, function () {
         $("#tweet-scroller > div").append($(this))
       })
+    }); 
   };
   
   function scrollTweets () {
@@ -44,13 +49,22 @@ $(function () {
       console.log ("pour wine");
       // Add Class to Rotate Wine Bottle           
       $("#bottle").toggleClass('pour');
-      $('#new-tweet').replaceWith(tweets.latest("div").attr('id', 'new-tweet'));
-      $('#new-tweet').addClass('pourmove');
+      $('#new-tweet').replaceWith(tweets.nextLatest("div").attr('id', 'new-tweet'));
+      $('#new-tweet').addClass('tweet-pour'); //TODO doesn't seem to do anything
+      $('#new-tweet').bind ('webkitAnimationEnd', function (e) {
+        // Set Tweets New Position in Class
+        console.log("Animation Pour End")
+        //$("#new-tweet").addClass('in-glass').toggleClass("tweet-pour")
+        $("#new-tweet").css({left: 646, top:462}).toggleClass("tweet-pour")
+      })
       // Fade in Tweet After Pour
       setTimeout (function () {
+        /*
         $('#new-tweet').fadeIn (2000).delay(4500).animate({left: 900, top: 0}, 2000, 'swing').fadeOut(500).css({left: 645, top: 460});
       }, 1500);
-      
+        */
+        $('#new-tweet').addClass("fly-away"); // USE CSS ANIMATION
+      },7500);
       // Rotate Bottle Back to Horizontal
       setTimeout (function () {
         $("#bottle").toggleClass('pour');
@@ -58,7 +72,23 @@ $(function () {
     }, pourEvery * 3000)
   }         
   
-       
+  function setupWineMenu () {
+    $(".wine-menu h4").click (function (e) {
+      var classes;
+      $('.wine-menu').toggleClass ("show-menu");
+      classes = $('.wine-menu').attr('class')
+      console.log ("Touched 'Now Pouring' " + classes);
+      
+    });
+    
+    $(".wine").click (function (e) {
+      var classes;
+      $(this).toggleClass ('show-info');
+      classes = $(this).attr('class');
+      console.log ("Touched Wine Bottle");
+     
+    })
+  }    
 
          
 });
--- public/javascripts/app/tweets.js
+++ public/javascripts/app/tweets.js
@@ -1,40 +1,72 @@
 var Tweets = Class.extend ({
-  init: function (data) {
-    if (typeof data == "String") {
-      this.tweets = $.parseJSON (data)
+  
+  
+  init: function (data, callback) {
+    this.nr = 0; // tracks the number of tweets collected from server
+    this.tweets = [ ];
+    if (data == 0) {
+      this.fetchMoreTweets (callback); // Get intial set of tweets
     } else {
-      this.tweets = data;
+      if (typeof data == "String") {
+        this.tweets = $.parseJSON (data)
+      } else {
+        this.tweets = data;
+      }
+      this.nr = this.number();
+      this.nextTweetNr = 0;
+      callback ('OK'); // data is available
     }
-    this.newestTweetNr = 0;
   },
   
-  oldest: function (nr, wrapper) {
+  oldestNTweets: function (nr, wrapper) {
     var $box, getNr, i, lastNr, tweet;
     var boxes = [ ];
     lastNr = Math.max (0, this.number() - nr);
     // Oldest at bottom of list
     for (i = this.tweets.length -1 ; i > lastNr; i--) {
       tweet = this.tweets[i];
-      boxes.push(this.buildTweet (tweet, wrapper));
+      boxes.push(this.buildTweet (tweet, wrapper, i));
     }
+    this.nextTweetNr = lastNr;
     return boxes;
   },
   
-  latest: function (wrapper) {
-    var newTweet = this.tweets[this.newestTweetNr]
-    this.newestTweetNr += 1;
-    if (this.newestTweetNr > 10) {
+  nextLatest: function (wrapper) {
+    var newTweet = this.tweets[this.nextTweetNr]
+    this.nextTweetNr -= 1; 
+    this.tweets.pop();     // Remove from end
+    if (this.nextTweetNr < 4) {
       //Need to go after a new set of tweets
-      this.newestTweetNr = 0;
+      this.nextTweetNr = 0;
+      //fetchMoreTweets (); // JSON request
     }
-    return this.buildTweet (newTweet, wrapper) 
+    return this.buildTweet (newTweet, wrapper, this.nextTweetNr) 
   },
   
-  buildTweet: function (tweet, wrapper) {
-    var $box;
+  addNewRemoveOldest: function () {
+   
+  },
+  
+  fetchMoreTweets: function (callback) {
+    var that = this;
+    $.getJSON("http://localhost:3000/billboard.json", function(data, status) {
+        console.log ("JSON fetched from server, status: " + status + " nr = " + data.length);
+
+        that.tweets = data.concat(that.tweets);
+        callback(status)
+    })
+  },
+  
+  buildTweet: function (tweet, wrapper, nr) {
+    var time, $box;
     $box = $("<" + wrapper + ">").addClass("tweet");
     $box.append ($('<img src="' + tweet.user.profile_image_url + '" />'));
+    //Fri Oct 29 16:40:25 +0000 2010
+    time = tweet.created_at.split(" ");
+    time = time[0] + " " + time[1] + " " + time[2] + " " + time[3];
     $box.append ($('<p>' + tweet.text + '</p>'));
+    $box.append ($('<p class="time">' + time + '</p>'));
+    $box.append ($('<p class="nr">' + nr + '</p>')); // add tweet number
     return $box
   },
   
