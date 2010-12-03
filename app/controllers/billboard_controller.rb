1d30d96e495c52c08591c68147943b0b1685bf62
Ken Rothmuller <krothmuller@gmail.com>
2010-12-03 14:23:19 -0800

    refactor controller

--- app/controllers/billboard_controller.rb
+++ app/controllers/billboard_controller.rb
@@ -2,7 +2,12 @@ class BillboardController < ApplicationController
   layout "billboard"
   
   def index
-    
+    @twitter_feed = TwitterFeed.first
+    client = @twitter_feed.client
+    @tweets = client.home_timeline
+  end
+  
+  def new_tweets
     @twitter_feed = TwitterFeed.first
     client = @twitter_feed.client
     @tweets = client.home_timeline
@@ -12,11 +17,6 @@ class BillboardController < ApplicationController
       format.json { render :layout => false ,
                            :json => @tweets.to_json }
     end
-    
-  end
-  
-  def update_tweets
-    
   end
   
   def initialize_tweets
