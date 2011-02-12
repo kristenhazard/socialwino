f2fe565857d78bf060a8bd58db91924983b0d469
Kristen Hazard <kristen@hazardbio.com>
2011-02-12 12:42:52 -0800

    add route for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Socialwino::Application.routes.draw do 
 
   devise_for :users
+
+  devise_scope :user do
+    get "login", :to => "devise/sessions#new"
+  end
+
   namespace :user do
     root :to => "users#index"
   end
